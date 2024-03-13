b4668f7781e0a0db96ec33c21fb7ca1888805ee1
kiarashsa84 <72kiarash84@gmail.com>
2024-03-13 23:07:50 +0330

    Team finished

--- CodeForces/src/Team.java
+++ CodeForces/src/Team.java
@@ -1,5 +1,25 @@
+import java.util.Scanner; 
+
 public class Team {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        int tedad_soal = input.nextInt(); 
+        int solved = 0; 
+        
+        for(int i = 0 ; i < tedad_soal; i++){
+            int result = 0; 
+            for (int j = 0; j < 3; j++){
+                int pos = input.nextInt(); 
+                if (pos == 1){
+                    result ++; 
+                }
+
+            }
+            if (result >= 2){
+                solved++; 
+            }
+        }
+
+        System.out.println(solved);
     }
 }
